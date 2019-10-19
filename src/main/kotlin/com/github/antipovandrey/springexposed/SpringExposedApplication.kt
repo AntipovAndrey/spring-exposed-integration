3c70c5eaf5136fc23fa1fb45dbbc7f02bc7c49bc
Antipov Andrey <andrey.antipov@jetbrains.com>
2019-10-19 13:06:06 +0300

    init

---
+++ src/main/kotlin/com/github/antipovandrey/springexposed/SpringExposedApplication.kt
@@ -0,0 +1,11 @@
+package com.github.antipovandrey.springexposed
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringExposedApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringExposedApplication>(*args)
+}
